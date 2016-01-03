e942f4fe6d4b30e727a6db4f27ba81965b7259e5
Vickenty Fesunov <kent@setattr.net>
2016-01-03 15:20:11 +0100

    Get rid of tabs.

--- src/lib.rs
+++ src/lib.rs
@@ -14,20 +14,20 @@ use raw::types::*;
 
 pub struct XS<'a> {
     #[cfg(perl_multiplicity)]
-	perl: *mut PerlInterpreter,
+    perl: *mut PerlInterpreter,
     #[allow(dead_code)]
     #[cfg(not(perl_multiplicity))]
     perl: (),
     #[allow(dead_code)]
-	cv: *mut CV,
-	stack: OuroborosStack,
-	marker: ::std::marker::PhantomData<&'a PerlInterpreter>,
+    cv: *mut CV,
+    stack: OuroborosStack,
+    marker: ::std::marker::PhantomData<&'a PerlInterpreter>,
 }
 
 impl<'a> XS<'a> {
 
-	#[cfg(perl_multiplicity)]
-	pub fn new(perl: *mut PerlInterpreter, cv: *mut CV) -> XS<'a> {
+    #[cfg(perl_multiplicity)]
+    pub fn new(perl: *mut PerlInterpreter, cv: *mut CV) -> XS<'a> {
         let stack = unsafe {
             let mut stack = mem::uninitialized();
             raw::funcs::ouroboros_stack_init(perl, &mut stack);
@@ -40,10 +40,10 @@ impl<'a> XS<'a> {
             stack: stack,
             marker: ::std::marker::PhantomData,
         }
-	}
+    }
 
-	#[cfg(not(perl_multiplicity))]
-	pub fn new(cv: *mut CV) -> XS<'a> {
+    #[cfg(not(perl_multiplicity))]
+    pub fn new(cv: *mut CV) -> XS<'a> {
         let stack = unsafe {
             let mut stack = mem::uninitialized();
             raw::funcs::ouroboros_stack_init(&mut stack);
@@ -56,36 +56,36 @@ impl<'a> XS<'a> {
             stack: stack,
             marker: ::std::marker::PhantomData,
         }
-	}
+    }
 
-	pub fn prepush(&mut self) {
-		unsafe {
-			ouroboros_stack_prepush!(self.perl, &mut self.stack);
-		}
-	}
+    pub fn prepush(&mut self) {
+        unsafe {
+            ouroboros_stack_prepush!(self.perl, &mut self.stack);
+        }
+    }
 
-	pub fn push_long(&mut self, val: IV) {
-		unsafe {
-			ouroboros_stack_xpush_iv!(self.perl, &mut self.stack, val);
-		}
-	}
+    pub fn push_long(&mut self, val: IV) {
+        unsafe {
+            ouroboros_stack_xpush_iv!(self.perl, &mut self.stack, val);
+        }
+    }
 
-	pub fn push_string(&mut self, string: &str) {
-		unsafe {
-			ouroboros_stack_xpush_pv!(self.perl, &mut self.stack, string.as_ptr() as *const i8, string.len() as STRLEN);
-		}
-	}
+    pub fn push_string(&mut self, string: &str) {
+        unsafe {
+            ouroboros_stack_xpush_pv!(self.perl, &mut self.stack, string.as_ptr() as *const i8, string.len() as STRLEN);
+        }
+    }
 
-	pub fn putback(&mut self) {
-		unsafe {
-			ouroboros_stack_putback!(self.perl, &mut self.stack);
-		}
-	}
+    pub fn putback(&mut self) {
+        unsafe {
+            ouroboros_stack_putback!(self.perl, &mut self.stack);
+        }
+    }
 
-	pub fn new_xs(&mut self, name: &str, xs: XSUBADDR_t, file: &'static [u8]) {
-		let cname = ffi::CString::new(name).unwrap();
-		unsafe {
-			newXS!(self.perl, cname.as_ptr(), xs, file.as_ptr() as *const c_char);
-		}
-	}
+    pub fn new_xs(&mut self, name: &str, xs: XSUBADDR_t, file: &'static [u8]) {
+        let cname = ffi::CString::new(name).unwrap();
+        unsafe {
+            newXS!(self.perl, cname.as_ptr(), xs, file.as_ptr() as *const c_char);
+        }
+    }
 }
--- src/macros.rs
+++ src/macros.rs
@@ -1,46 +1,46 @@
 #[cfg(perl_multiplicity)]
 #[macro_export]
 macro_rules! xs_proc {
-	($name:ident, $xs:ident, $body:block) => {
-		#[allow(dead_code)]
-		#[allow(non_snake_case)]
-		#[no_mangle]
-		pub extern "C" fn $name(perl: *mut $crate::raw::types::PerlInterpreter, cv: *mut $crate::raw::types::CV) {
-			let mut $xs = $crate::XS::new(perl, cv);
-			$body
-		}
-	}
+    ($name:ident, $xs:ident, $body:block) => {
+        #[allow(dead_code)]
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $name(perl: *mut $crate::raw::types::PerlInterpreter, cv: *mut $crate::raw::types::CV) {
+            let mut $xs = $crate::XS::new(perl, cv);
+            $body
+        }
+    }
 }
 
 #[cfg(not(perl_multiplicity))]
 #[macro_export]
 macro_rules! xs_proc {
-	($name:ident, $xs:ident, $body:block) => {
-		#[allow(dead_code)]
-		#[allow(non_snake_case)]
-		#[no_mangle]
-		pub extern "C" fn $name(cv: *mut $crate::raw::types::CV) {
-			let mut $xs = $crate::XS::new(cv);
-			$body
-		}
-	}
+    ($name:ident, $xs:ident, $body:block) => {
+        #[allow(dead_code)]
+        #[allow(non_snake_case)]
+        #[no_mangle]
+        pub extern "C" fn $name(cv: *mut $crate::raw::types::CV) {
+            let mut $xs = $crate::XS::new(cv);
+            $body
+        }
+    }
 }
 
 #[macro_export]
 macro_rules! XS {
-	(package $pkg:ident ($boot:ident) { $( sub $name:ident($xs:ident) $body:block)* }) => {
-		$(xs_proc!($name, $xs, $body);)*
+    (package $pkg:ident ($boot:ident) { $( sub $name:ident($xs:ident) $body:block)* }) => {
+        $(xs_proc!($name, $xs, $body);)*
 
-		xs_proc!(
-			$boot, xs, {
-			$({
-				let name = [ stringify!($pkg), stringify!($name) ].join("::");
-				xs.new_xs(&name, $name, b"Rust code\0");
-			})*
+        xs_proc!(
+            $boot, xs, {
+            $({
+                let name = [ stringify!($pkg), stringify!($name) ].join("::");
+                xs.new_xs(&name, $name, b"Rust code\0");
+            })*
 
-			xs.prepush();
-			xs.push_long(1);
-			xs.putback();
-		});
-	}
+            xs.prepush();
+            xs.push_long(1);
+            xs.putback();
+        });
+    }
 }
