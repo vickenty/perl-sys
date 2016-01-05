19d52297b9983d0f42365ddcc55b825e1f32ab9b
Vickenty Fesunov <kent@setattr.net>
2016-01-05 22:18:19 +0100

    Add a test.
    This is pretty much the only thing that can be tested without actually
    linking to the perl binary.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 include!(concat!(env!("OUT_DIR"), "/perl_defs.rs"));
+
+#[test]
+fn test_svttype() {
+    use consts::*;
+    /* These relations are documented in perlguts */
+    assert!(consts::SVt_PVIV < SVt_PVAV);
+    assert!(consts::SVt_PVMG < SVt_PVAV);
+}
