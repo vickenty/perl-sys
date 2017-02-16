c86813709de96c3e3ea63693798011caddbfded3
Vickenty Fesunov <kent@setattr.net>
2017-02-16 19:00:40 +0100

    Simplify pthx macro.
    Instead of having two branches to handle declarations and calls with and
    without additional parameters, use one that can handle both cases.

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,19 @@ include!(concat!(env!("OUT_DIR"), "/perl_sys.rs"));
 #[cfg(perl_multiplicity)]
 #[macro_export]
 macro_rules! pthx {
-    ($( #[$me:meta] )* fn $id:ident ( $ctx:ident ) $body:block) => ($( #[$me] )* pub extern "C" fn $id ($ctx: *mut $crate::types::PerlInterpreter) $body);
-    ($( #[$me:meta] )* fn $id:ident ( $ctx:ident, $( $pid:ident : $pty:ty ),* ) $body:block) => ($( #[$me] )* pub extern "C" fn $id ($ctx: *mut $crate::types::PerlInterpreter, $( $pid : $pty ),*) $body);
+    ($( #[$me:meta] )* fn $id:ident ( $ctx:ident $(, $pid:ident : $pty:ty )* ) $body:block) => ($( #[$me] )* pub extern "C" fn $id ($ctx: *mut $crate::types::PerlInterpreter $(, $pid : $pty )*) $body);
+    ($( #[$me:meta] )* fn $id:ident ( $ctx:ident $(, $pid:ident : $pty:ty )* ) -> $rty:ty $body:block) => ($( #[$me] )* pub extern "C" fn $id ($ctx: *mut $crate::types::PerlInterpreter $(, $pid : $pty )*) -> $rty $body);
 
-    ($( #[$me:meta] )* fn $id:ident ( $ctx:ident ) -> $rty:ty $body:block) => ($( #[$me] )* pub extern "C" fn $id ($ctx: *mut $crate::types::PerlInterpreter) -> $rty $body);
-    ($( #[$me:meta] )* fn $id:ident ( $ctx:ident, $( $pid:ident : $pty:ty ),* ) -> $rty:ty $body:block) => ($( #[$me] )* pub extern "C" fn $id ($ctx: *mut $crate::types::PerlInterpreter, $( $pid : $pty ),*) -> $rty $body);
-
-    ($id:ident ( $ctx:expr )) => ($id($ctx));
-    ($id:ident ( $ctx:expr, $( $p:expr ),* $(,)* )) => ($id($ctx, $( $p ),*));
+    ($id:ident ( $ctx:expr $(, $p:expr )* $(,)* )) => ($id($ctx $(, $p )*));
 }
 
 #[cfg(not(perl_multiplicity))]
 #[macro_export]
 macro_rules! pthx {
-    ($(#[$me:meta])* fn $id:ident ( $ctx:ident ) -> $rty:ty $body:block) => ($(#[$me])* pub extern "C" fn $id () -> $rty { let $ctx = (); $body });
-    ($(#[$me:meta])* fn $id:ident ( $ctx:ident, $( $pid:ident : $pty:ty ),* ) -> $rty:ty $body:block) => ($(#[$me])* pub extern "C" fn $id ($( $pid : $pty ),*) -> $rty { let $ctx = (); $body });
+    ($(#[$me:meta])* fn $id:ident ( $ctx:ident $(, $pid:ident : $pty:ty )* ) -> $rty:ty $body:block) => ($(#[$me])* pub extern "C" fn $id ($( $pid : $pty ),*) -> $rty { let $ctx = (); $body });
+    ($(#[$me:meta])* fn $id:ident ( $ctx:ident $(, $pid:ident : $pty:ty )* ) $body:block) => ($(#[$me])* pub extern "C" fn $id ($( $pid : $pty ),*) { let $ctx = (); $body });
 
-    ($(#[$me:meta])* fn $id:ident ( $ctx:ident ) $body:block) => ($(#[$me])* pub extern "C" fn $id () { let $ctx = (); $body });
-    ($(#[$me:meta])* fn $id:ident ( $ctx:ident, $( $pid:ident : $pty:ty ),* ) $body:block) => ($(#[$me])* pub extern "C" fn $id ($( $pid : $pty ),*) { let $ctx = (); $body });
-
-    ($id:ident ( $ctx:expr )) => ($id());
-    ($id:ident ( $ctx:expr, $( $p:expr ),* $(,)* )) => ($id($( $p ),*));
+    ($id:ident ( $ctx:expr $(, $p:expr ),* $(,)* )) => ($id($( $p ),*));
 }
 
 #[test]
